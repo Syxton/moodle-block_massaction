b4dab6f7ba971ca0bf2f266056f883d405ed7c4e
PhMemmel <65113153+PhMemmel@users.noreply.github.com>
2026-05-22 06:15:01 +0200

    Update minimum moodle version to moodle 5.2, bump for release v8.0.1

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2026042700;
-$plugin->requires  = 2024042000;
+$plugin->version = 2026052200;
+$plugin->requires  = 2026042000;
 $plugin->component = 'block_massaction';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v8.0.0';
+$plugin->release = 'v8.0.1';
 $plugin->supported = [502, 502];
