684fdb80b52f3cd9c99dc0d0529dd61ede57ec54
Matt Davidson <davidso1@rose-hulman.edu>
2026-09-14 16:29:06 -0400

    Update plugin version and release number

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2025102700;
+$plugin->version = 2025102701;
 $plugin->requires  = 2023100900;
 $plugin->component = 'block_massaction';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v7.5.1';
+$plugin->release = 'v7.5.2';
 $plugin->supported = [500, 501];
